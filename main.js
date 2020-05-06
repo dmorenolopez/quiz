f014ffe184e297f6678b12ae79c594f9324c2db9
Diddier <shoesoffsas@gmail.com>
2020-05-05 20:29:35 -0500

    Este es el primer commit del quiz casa de Papel

---
+++ main.js
@@ -0,0 +1,8 @@
+function check(){
+
+	var question1 = document.quiz.question1.value;
+	var question2 = document.quiz.question2.value;
+    var question3 = document.quiz.question3.value;
+    var question4 = document.quiz.question3.value;
+    var question5 = document.quiz.question3.value;
+	var correct = 0;
